a33cd53e060cd5432ac6f447e4367249e17b0ff2
scottwhendrickson <scottwhendrickson@gmail.com>
2020-03-14 11:22:42 -0400

    initial

---
+++ simpleProject.js
@@ -0,0 +1,15 @@
+exports.handler = async (event) => {
+    let AWS = require('aws-sdk');
+exports.handler = function(event, context, callback) {
+
+    let receiver = event['receiver'];
+    let sender = event['sender'];
+    let message = event['message'];
+
+    console.log("Sending message", message, "to receiver", receiver);
+
+    callback(null,'Successfully executed');
+}
+    
+    return {"message": "Successfully executed"};
+};
